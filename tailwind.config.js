203ee547c0ef9db89cec37a1af89daa2b1f2313a
Emil Forsén <emilxforsen@gmail.com>
2024-05-06 20:36:32 +0200

    Added Losing My Marbles project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,13 @@ module.exports = {
   content: ["./**/*.html", "./_includes/**/*.html", "./_layouts/**/*.html", "./_posts/**/*.{html,md}", "./_projects/**/*.{html,md}"],
   theme: {
     extend: {
-    
       fontFamily: {
         //set papyrus as a font option
           'sans': ['Inter', 'Papyrus', 'sans-serif']
       }
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+},
+plugins: [
+  require('@tailwindcss/typography'),
+],
 }
